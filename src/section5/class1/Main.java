4cf9df07171152b2b086e29f5c5a7e7cc47025af
stk346 <stk346@gamil.com>
2024-03-05 18:20:16 +0900

    section5-class1

---
+++ src/section5/class1/Main.java
@@ -0,0 +1,51 @@
+package section5.class1;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String input = kb.nextLine();
+        System.out.println(solution(input));
+    }
+
+    private static String solution(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) {
+            return "NO";
+        }
+        return "YES";
+    }
+
+    private static String myTry(String input) {
+        int bracketOffset = 0;
+        if (input.charAt(0) == ')' || input.charAt(input.length() - 1) == '(') {
+            return "NO";
+        }
+        for (char c : input.toCharArray()) {
+            if (c == '(') {
+                bracketOffset++;
+            }
+            if (c == ')') {
+                bracketOffset--;
+            }
+        }
+        if (bracketOffset == 0) {
+            return  "YES";
+        } else {
+            return "NO";
+        }
+    }
+}
